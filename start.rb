57127d262a4e9673ce40a4aa78fa24a7ae178775
Abdelrahman <abdelrahman.hosny@hotmail.com>
2015-07-04 05:32:05 -0400

    removed Task.rb

--- start.rb
+++ start.rb
@@ -17,7 +17,6 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 =end
 
-require_relative './lib/Task.rb'
 require_relative './lib/Algo.rb'
 require_relative './lib/Server.rb'
 
