a260895b7d4a4b871f03b1385fff83f8a5da5d3f
Abdelrahman <abdelrahman.hosny@hotmail.com>
2015-07-31 14:47:22 -0400

    add scroll up

--- web/js/params.js
+++ web/js/params.js
@@ -409,3 +409,21 @@ $('.dropup.keep-open').on({
     "hide.bs.dropdown":  function() { return this.closable; }
 });
 update_ui();
+$(document).ready(function(){
+ 
+        //When distance from top = 250px fade button in/out
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 250) {
+                $('#scrollup').fadeIn(300);
+            } else {
+                $('#scrollup').fadeOut(300);
+            }
+        });
+ 
+        //On click scroll to top of page t = 1000ms
+        $('#scrollup').click(function(){
+            $("html, body").animate({ scrollTop: 0 }, 1000);
+            return false;
+        });
+ 
+});
