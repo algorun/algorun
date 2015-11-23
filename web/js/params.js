391c237bd28a2b4adf8fc3fde9f1ca3e083340cd
Abdelrahman <abdelrahman.hosny@hotmail.com>
2015-11-22 20:47:24 -0500

    ui bug fixes

--- web/js/params.js
+++ web/js/params.js
@@ -407,11 +407,15 @@ function update_ui(){
                 var author = parse("<div class='col-md-1'><img id='%s' src='/images/author.jpeg' alt='author picture' style='width:40px; height:40px; vertical-align: middle;'></div><div class='col-md-5'><a href='%s' target='_blank'><h5 style='line-height: 30%;'>%s</h5></a><a href='%s' target='_blank' style='color: #888888; font-size:10px;'><p style='line-height: 100%;'>%s</p></a></div>", "auth"+auth_id ,personal_page, name, org_page, org);
                 // get image from gravatar
                 $("#authors").append( author );
-                if(pic.trim() === ''){
-                    $("#auth" + auth_id).attr('src', 'http://www.gravatar.com/avatar/' + md5(email));
-                }else{
-                    $("#auth" + auth_id).attr('src', '/algorun_info/' + pic.trim());
-                }
+		if(pic != undefined){
+                    if(pic.trim() === ''){
+                        $("#auth" + auth_id).attr('src', 'http://www.gravatar.com/avatar/' + md5(email));
+                    }else{
+                        $("#auth" + auth_id).attr('src', '/algorun_info/' + pic.trim());
+                    }
+		}else{
+		    $("#auth" + auth_id).attr('src', 'http://www.gravatar.com/avatar/' + md5(email));		
+		}
             });
         }
         
