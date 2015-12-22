de83747c6bbcd4595255d466f8323c8e2989a60d
Abdelrahman <abdelrahman.hosny@hotmail.com>
2015-12-22 15:21:20 -0500

    moved skeleton to a separate repository

--- skeleton/README.md
+++
@@ -1,2 +0,0 @@
-Use this directory as a template to create your Algorun container.
-For more information about how to do it, please refer to our [How-to Guide](http://algorun.org/how-to.html)
--- skeleton/src/test.rb
+++
@@ -1,8 +0,0 @@
-input_file = ARGV[0].dup
-fr = File.open(input_file, "r")
-fw = File.open("output.txt", "w")
-fr.each_line do |line|
-	fw.write(line + " added for test")
-end
-fr.close()
-fw.close()
